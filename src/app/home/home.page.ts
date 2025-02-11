b98246efcb27a2b4f288e34d1466fcc872126a55
EvanGod <2022171006@uteq.edu.mx>
2025-02-11 11:00:49 -0600

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,37 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  messageVisible = false;
+  username: string = '';
+  password: string = '';
+  isValid = false;
 
-  constructor() {}
+  constructor(private alertController: AlertController, private navCtrl: NavController) {}
 
+  showMessage() {
+    this.messageVisible = true;
+    setTimeout(() => {
+      this.messageVisible = false;
+    }, 5000);
+  }
+
+  goToRegister() {
+    this.navCtrl.navigateForward('/register');
+  }
+  
+  validateFields() {
+    this.username = this.username.toLowerCase().trim();
+    this.password = this.password.trim();
+    this.isValid = this.username !== '' && this.password !== '' && !this.username.includes(' ') && !this.password.includes(' ');
+  }
+
+  async showModal() {
+    const alert = await this.alertController.create({
+      header: 'Información ingresada',
+      message: `Username: ${this.username}      Password: ${this.password}`,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 }
