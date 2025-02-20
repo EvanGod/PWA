11d001347e73ed3b9b5a5290382d1e161d021944
EvanGod <2022171006@uteq.edu.mx>
2025-02-20 14:46:45 -0600

    [Add] -Connection to DB Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAkW9ff_C0akHA6AuQv1g7OkaLOf7aGVj4",
+    authDomain: "pwa-evangod.firebaseapp.com",
+    databaseURL: "https://pwa-evangod-default-rtdb.firebaseio.com",
+    projectId: "pwa-evangod",
+    storageBucket: "pwa-evangod.firebasestorage.app",
+    messagingSenderId: "1003770529231",
+    appId: "1:1003770529231:web:b662d50ab795b7c97c3eba",
+    measurementId: "G-2G6QPW0NWX"
+  }
 };
